7938221f4e0a588f01dbe7f3785d7f62929bdcea
DziyanaLysak <pandorum888@gmail.com>
2025-02-16 11:04:36 +0300

    M-8-4

---
+++ 12345.py
@@ -0,0 +1,18 @@
+
+# Напиши функцию, которая печатает все четные числа от 1 до 20.
+
+a = [1, 2, 3, 4]
+d = 2
+c = False
+
+for x in a:
+    if x == d:
+        c = True
+    break
+if c:
+    print("ok")
+else:
+    print("no")    
+
+
+    
--- lesson8/4.py
+++ lesson8/4.py
@@ -10,3 +10,42 @@
 с помощью try перехватить возможные ошибки.
 '''
 
+"""
+    Функция monetary: Преобразует число в денежный формат: разделители пробелами, копейки, "руб.".
+    Например: 1234567 -> "1 234 567.00 руб."
+
+    number: Число (int или float).
+
+"""
+def monetary(number):
+    
+    try:
+    
+        if not isinstance(number, (int, float)):
+            return "Ошибка: Введите число!"
+
+        integer_part = int(number) 
+        pennies = int(round((number - integer_part) * 100))  
+
+        pennies_str = str(pennies).zfill(2)  
+
+        integer_part_formatted = ""
+        categories = str(integer_part)[::-1]  
+        for i, digit in enumerate(categories):
+            integer_part_formatted += digit
+            if (i + 1) % 3 == 0 and i != len(categories) - 1:  
+                integer_part_formatted += " "
+
+        integer_part_formatted = integer_part_formatted[::-1] 
+
+        result = integer_part_formatted + "." + pennies_str + " руб."
+
+        return result
+
+    except Exception as exception:
+        return f"Произошла ошибка: {exception}"
+
+print(monetary(1234567)) # Вывод: 1 234 567.00 руб.
+print(monetary("a")) # Вывод: Ошибка: Введите число!
+
+
